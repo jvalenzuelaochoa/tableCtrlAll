306b95760a2e026741c4ebccd2e3941567648c4d
jvalenzu <jvalenzu@javiers-mbp>
2019-04-25 00:39:46 -0500

    Initial benchmark for dynamoDB project
    Change-Id: Ibcaba2dee406bc84b67676911260d1616af255a5

---
+++ src/main/java/project/benchmark/MyBenchmark.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2014, Oracle America, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Oracle nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package project.benchmark;
+
+import org.openjdk.jmh.annotations.*;
+
+import project.dynamodb.AWSDynamoDB;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@State(Scope.Benchmark)
+
+
+public class MyBenchmark {
+    
+//	private int testTime;
+
+    @Param({
+            "Users_10",
+            "Users_10000"
+    })
+    public String tableName;
+
+    @Setup
+    public void loadTime(){
+    	String hostname = "http://localhost:8000";
+    	String signingRegion = "us-east-1";
+
+    	AWSDynamoDB dynamoDBHelper = new AWSDynamoDB(hostname, signingRegion);
+
+    	List<String> tableAttributes = new ArrayList<String>();
+    	tableAttributes.add("id");
+
+    	try {
+    		//Check if table exists already
+    		if(!dynamoDBHelper.doesTableExist(tableName)) {
+    			dynamoDBHelper.createTable(tableName, tableAttributes);
+    		}
+    		else {
+    			System.out.println("Table exists. Skipping Table creation.");
+    		}
+    		//Save data in python using batch write
+//    		dynamoDBHelper.saveData(tableName, primaryKey, imageInfoHashMap);
+    	}
+        catch (Exception e) {
+            System.err.println("DynamoDB Exception: ");
+            System.err.println(e.getMessage());
+        }
+    	
+//    	testTime = Integer.parseInt(sleepTime);
+    }
+
+	@Benchmark
+    public void testMethod() {
+        System.out.println(tableName);
+    }
+
+//	@Benchmark
+//    public void alternateTest() {
+//        doMagic(testTime);
+//    }
+//    
+    public static void doMagic(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException ignored) {
+        }
+    }
+}
---
+++ src/main/java/project/dynamodb/App.java
@@ -0,0 +1,81 @@
+package project.dynamodb;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.json.JSONObject;
+
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+import com.amazonaws.services.dynamodbv2.document.ScanOutcome;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    	String hostname = "http://localhost:8000";
+    	String signingRegion = "us-east-1";
+
+    	String tableName = "Users_10000";
+    	
+    	AWSDynamoDB dynamoDBHelper = new AWSDynamoDB(hostname, signingRegion);
+
+    	List<String> tableAttributes = new ArrayList<String>();
+    	tableAttributes.add("id");
+    	
+    	
+    	String primaryKey = "id";
+    	    	
+    	try {
+    		//Check if table exists already
+    		if(!dynamoDBHelper.doesTableExist(tableName)) {
+    			dynamoDBHelper.createTable(tableName, tableAttributes);
+    		}
+    		else {
+    			System.out.println("Table exists. Skipping Table creation.");
+    		}
+    		//Save data in python using batch write
+//    		dynamoDBHelper.saveData(tableName, primaryKey, imageInfoHashMap);
+    	}
+        catch (Exception e) {
+            System.err.println("DynamoDB Exception: ");
+            System.err.println(e.getMessage());
+        }
+    	
+//    	System.out.println("Query Item: \n");
+//    	
+//		Item item = new Item();
+//		item = null;
+//    	String primaryKeyId = "id";
+//    	String primaryKeyValue = "52023020";
+//    	
+//    	try {
+//    		item = dynamoDBHelper.getTableItem(tableName, primaryKeyId, primaryKeyValue);
+//    	}
+//    	catch (Exception e) {
+//            System.err.println("DynamoDB Exception: ");
+//            System.err.println(e.getMessage());
+//    	}
+    	
+        try {
+        	String filterExpression = "salary > :val";
+        	String attributeValue = "388287";
+        	ScanResult result = dynamoDBHelper.scanAndFilterTable(tableName, attributeValue, filterExpression);
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to scan the table:");
+            System.err.println(e.getMessage());
+        }
+    	
+    }
+}
